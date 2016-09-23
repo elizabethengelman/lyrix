258740f43b2e90132d3a0822147f10a84a0bcf83
Elizabeth Engelman <emengelman@gmail.com>
2016-09-22 21:32:08 -0500

    Cleans up ResultsFormatter

--- helpers/result_formatter.rb
+++ helpers/result_formatter.rb
@@ -4,14 +4,13 @@ class ResultFormatter
   attr_reader :parsed_response
   FIELDS_TO_DISPLAY = [ "track_name", "artist_name", "explicit"]
 
-  def initialize( response )
-    # turn the result into a class?
-    @parsed_response = JSON.parse( response.body )
+  def initialize(response)
+    @parsed_response = JSON.parse(response.body)
   end
 
   def format
     track_list.map do |track_listing|
-      track_listing["track"].select{ |key, value| FIELDS_TO_DISPLAY.include?( key ) }
+      track_listing["track"].select{ |key, value| FIELDS_TO_DISPLAY.include?(key) }
     end
   end
 
